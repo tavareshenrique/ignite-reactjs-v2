696d1dc4b4f1ae6867e60cb74cff916a2385da55
Henrique Tavares <ihenrits@gmail.com>
2022-06-02 19:26:13 -0300

    feat: componente Header

--- 01-ignite-feed/src/App.jsx
+++ 01-ignite-feed/src/App.jsx
@@ -1,10 +1,10 @@
+import Header from './components/Header'
+
 import './global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Header />
   )
 }
 
