63cf969231e47c46eb566e840d5ecbcb56e278ea
Henrique Tavares <ihenrits@gmail.com>
2022-06-02 19:58:33 -0300

    chore: finalizando sidebar

--- 01-ignite-feed/src/components/Sidebar.jsx
+++ 01-ignite-feed/src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
-import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
@@ -8,12 +10,19 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
+        <img 
+          src="https://avatars.githubusercontent.com/u/27022914?v=4" 
+          alt="Henrique Tavares" 
+          className={styles.avatar}
+        />
+
         <strong>Henrique Tavares</strong>
         <span>Front-End Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
