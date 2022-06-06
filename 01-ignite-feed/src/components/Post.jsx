07077a21274ce8de5db0a7f47a4859d1f10c3980
Henrique Tavares <ihenrits@gmail.com>
2022-06-06 18:49:31 -0300

    chore: estilizações do Post

--- 01-ignite-feed/src/components/Post.jsx
+++ 01-ignite-feed/src/components/Post.jsx
@@ -25,10 +25,20 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="#">👉 jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#novoprojeto">#novoprojeto</a>
+          {' '}
+          <a href="#nlw">#nlw</a>
+          {' '}
+          <a href="#rocketseat">#rocketseat</a>
+        </p>
+        {/* <p><a href="#">👉 jane.design/doctorcare</a></p>
         <p><a href="#novoprojeto">#novoprojeto</a></p>
         <p><a href="#nlw">#nlw</a></p>
-        <p><a href="#rocketseat">#rocketseat</a></p>
+        <p><a href="#rocketseat">#rocketseat</a></p> */}
       </div>
     </article>
   )
