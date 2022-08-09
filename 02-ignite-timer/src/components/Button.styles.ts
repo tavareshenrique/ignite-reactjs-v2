62994846fa317155f06c9168ced832db4cb55ba8
Henrique Tavares <ihenrits@gmail.com>
2022-08-08 21:50:50 -0300

    implements styled components

---
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface IButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariant = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
+
+export const ButtonContainer = styled.button<IButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${props => buttonVariant[props.variant]};
+`;
---
+++ 02-ignite-timer/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+interface IButtonProps {
+  variant?: ButtonVariant;
+}
+
+export function Button({  variant = 'primary' }: IButtonProps) {
+  return (
+    <ButtonContainer variant={variant}>
+      Enviar
+    </ButtonContainer>
+  );
+}
