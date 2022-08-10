4dd159721ad1a56c07ffec8a107006486592d683
Henrique Tavares <ihenrits@gmail.com>
2022-08-10 18:56:05 -0300

    work with react router dom

--- 02-ignite-timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles";
-
-interface IButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: IButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
