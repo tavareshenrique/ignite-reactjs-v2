d6de21289422d1322de13d0f67fe16170844b4cc
Henrique Tavares <ihenrits@gmail.com>
2022-08-23 17:50:59 -0300

    formatting date

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,5 +1,8 @@
 import { useContext } from "react";
 
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { CyclesContext } from "../../contexts/CyclesContext";
 
 import { HistoryContainer, HistoryList, Status } from "./styles";
@@ -26,7 +29,12 @@ export default function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>
                   {cycle.finishedDate && (
                     <Status statusColor="green">Finalizado</Status>
