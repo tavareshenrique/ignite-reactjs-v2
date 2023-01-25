c4799faea8c2162a39bdc8265f547219c3620781
Henrique Tavares <ihenrits@gmail.com>
2023-01-24 21:27:05 -0300

    Tabela de Transações

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,10 +1,38 @@
 import { Header, Summary } from '../../components';
 
-export function Transactions () {
-  return (
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
+
+export function Transactions() {
+	return (
 		<div>
 			<Header />
 			<Summary />
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant='income'>
+                  R$ 12.000,00
+								</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>13/01/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Hamburguer</td>
+							<td>
+								<PriceHighlight variant='outcome'>
+                  -R$ 59,90
+								</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>12/01/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</div>
-  );
+	);
 }
