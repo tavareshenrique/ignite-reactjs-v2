3f9e7c3dee602d026c5c36c9d74542f170181a69
Henrique Tavares <ihenrits@gmail.com>
2023-01-24 21:55:58 -0300

    Criando Modal Acessível

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/logo.svg';
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 export function Header () {
-  return (
+	return (
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoImg} alt="" />
 
-				<NewTransactionButton type="button">Nova Transação</NewTransactionButton>
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton type="button">
+              Nova Transação
+						</NewTransactionButton>
+					</Dialog.Trigger>
+
+					<Dialog.Portal>
+						<Dialog.Overlay />
+
+						<Dialog.Content>
+							<Dialog.Title>Nova Transação</Dialog.Title>
+							<Dialog.Close />
+						</Dialog.Content>
+					</Dialog.Portal>
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
-  );
+	);
 }
