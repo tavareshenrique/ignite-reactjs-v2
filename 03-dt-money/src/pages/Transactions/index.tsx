cdfa779220508e571da39a9a6e348df15e068a4c
Henrique Tavares <ihenrits@gmail.com>
2023-01-30 19:56:11 -0300

    realizando requisição HTTP

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Header, Summary, SearchForm } from '../../components';
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
 
+type TTransactionType = 'income' | 'outcome';
+
+interface ITransaction {
+  id: number;
+  description: string;
+  type: TTransactionType;
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+	const [transactions, setTransactions] = useState<ITransaction[] | null>(null);
+
+	async function loadTransactions() {
+		const response = await fetch('http://localhost:3333/transactions');
+
+		const data = await response.json();
+
+		setTransactions(data);
+	}
+
+	useEffect(() => {
+		loadTransactions();
+	}, []);
+
 	return (
 		<div>
 			<Header />
@@ -13,26 +39,18 @@ export function Transactions() {
 
 				<TransactionsTable>
 					<tbody>
-						<tr>
-							<td width="50%">Desenvolvimento de site</td>
-							<td>
-								<PriceHighlight variant='income'>
-                  R$ 12.000,00
-								</PriceHighlight>
-							</td>
-							<td>Venda</td>
-							<td>13/01/2023</td>
-						</tr>
-						<tr>
-							<td width="50%">Hamburguer</td>
-							<td>
-								<PriceHighlight variant='outcome'>
-                  -R$ 59,90
-								</PriceHighlight>
-							</td>
-							<td>Venda</td>
-							<td>12/01/2023</td>
-						</tr>
+						{transactions?.map((transaction) => (
+							<tr key={transaction.id} >
+								<td width="50%">{transaction.description}</td>
+								<td>
+									<PriceHighlight variant='income'>
+										{transaction.price}
+									</PriceHighlight>
+								</td>
+								<td>{transaction.category}</td>
+								<td>{transaction.createdAt}</td>
+							</tr>
+						))}
 					</tbody>
 				</TransactionsTable>
 			</TransactionsContainer>
