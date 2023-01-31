6d8a8123f01ea3248ea9dd235d4c1f94f3856c79
Henrique Tavares <ihenrits@gmail.com>
2023-01-30 21:01:40 -0300

    criando hook para o resumo

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -1,32 +1,13 @@
-import { useContext } from 'react';
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
-
-import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 import { priceFormatter } from '../../services/format/currency';
 
+import { useSummary } from '../../hooks/useSummary';
+
 import { SummaryCard, SummaryContainer } from './styles';
 
-const INITIAL_VALUE = {
-	income: 0,
-	outcome: 0,
-	total: 0
-};
-
 export function Summary () {
-	const { transactions } = useContext(TransactionsContext);
-
-	const summary = transactions?.reduce((acc, transaction) => {
-		if (transaction.type === 'income') {
-			acc.income += transaction.price;
-			acc.total += transaction.price;
-		} else {
-			acc.outcome += transaction.price;
-			acc.total -= transaction.price;
-		}
-
-		return acc;
-	}, INITIAL_VALUE);
+	const summary = useSummary();
 
 	return (
 		<SummaryContainer>
