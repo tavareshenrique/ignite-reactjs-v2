aff31c942544a46524d04fd42ff24a21d2e077fd
Henrique Tavares <ihenrits@gmail.com>
2023-05-09 21:38:26 -0300

    using next image

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { AppProps } from 'next/app';
+import Image from 'next/image';
+
+import logoImg from '../assets/logo.svg';
 
 import { globalStyles } from '../styles/global';
+
+import { Container, Header } from '../styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
 }
