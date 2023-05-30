0cead96207030593a3244d2fa598e85bec199d96
Henrique Tavares <ihenrits@gmail.com>
2023-05-30 08:43:34 -0300

    request to stripe api and list products

--- 04-ignite-shop/next.config.js
+++ 04-ignite-shop/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    domains: ['files.stripe.com'],
+  },
 };
 
 module.exports = nextConfig;
--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 
+import Stripe from 'stripe';
+
 import { useKeenSlider } from 'keen-slider/react';
+
+import { stripe } from '../lib/stripe';
 
 import ts1Image from '../assets/ts-1.png';
 import ts2Image from '../assets/ts-2.png';
@@ -10,7 +15,16 @@ import { HomeContainer, Product } from '../styles/pages/home';
 
 import 'keen-slider/keen-slider.min.css';
 
-export default function Home() {
+interface IHomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+export default function Home({ products }: IHomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -20,50 +34,44 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image
-          src={ts1Image}
-          alt="Camiseta da Maratona Explorer - Modelo Preto com a imagem de um Astronauta no centro escrito Maratona Explorer por cima do Astronauta"
-          title="Camiseta da Maratona Explorer - Modelo Preto com a imagem de um Astronauta no centro escrito Maratona Explorer por cima do Astronauta"
-          width={520}
-          height={520}
-        />
+      {products.map((product) => (
+        <Product className="keen-slider__slide" key={product.id}>
+          <Image
+            src={product.imageUrl}
+            alt="Camiseta da Maratona Explorer - Modelo Preto com a imagem de um Astronauta no centro escrito Maratona Explorer por cima do Astronauta"
+            title="Camiseta da Maratona Explorer - Modelo Preto com a imagem de um Astronauta no centro escrito Maratona Explorer por cima do Astronauta"
+            width={520}
+            height={520}
+          />
 
-        <footer>
-          <strong>Camiseta Maratona Explorer</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image
-          src={ts2Image}
-          alt="Camiseta Ignite Lab ReactJS - Modelo Preto com a imagem da logo do React com os dizeres logo abaixo escrito: Ignite Lab ReactJS"
-          title="Camiseta Ignite Lab ReactJS - Modelo Preto com a imagem da logo do React com os dizeres logo abaixo escrito: Ignite Lab ReactJS"
-          width={520}
-          height={520}
-        />
-
-        <footer>
-          <strong>Camiseta Ignite Lab ReactJS</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image
-          src={ts3Image}
-          alt="Camiseta Ignite Aboard - Modelo Preto com a imagem de um capacete de astronauta com os dizeres por cima dele escrito Ignite Aboard"
-          title="Camiseta Ignite Aboard - Modelo Preto com a imagem de um capacete de astronauta com os dizeres por cima dele escrito Ignite Aboard"
-          width={520}
-          height={520}
-        />
-
-        <footer>
-          <strong>Camiseta Ignite Aboard</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+      ))}
     </HomeContainer>
   );
 }
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
