e43b9470bada7e94fd0a5baa1e76aaafae55204c
Henrique Tavares <ihenrits@gmail.com>
2023-05-31 09:16:38 -0300

    product page structure

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product';
 
 export default function Product() {
-  const { query } = useRouter();
+  // const { query } = useRouter();
 
-  return <h1>Product {query.id}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+          aliquid rerum exercitationem facere a molestiae ut sed velit non
+          mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed
+          laboriosam tempora?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
