4c4fee2eb261a6470a2f87e02bd458195e821c70
Henrique Tavares <ihenrits@gmail.com>
2023-06-14 09:03:43 -0300

    configuring stitches

---
+++ 05-design-system/packages/react/src/styles/index.ts
@@ -0,0 +1,38 @@
+import { createStitches, defaultThemeMap } from '@stitches/react';
+
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens';
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+});
