f4e84576cba1b91a4a5113f8778d625e70332792
Henrique Tavares <ihenrits@gmail.com>
2023-06-14 10:00:46 -0300

    creating Button story

---
+++ 05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react';
+
+import { Button, TButtonProps } from '@ignite-ui/react';
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} satisfies Meta<TButtonProps>;
+
+export const Primary: StoryObj<TButtonProps> = {};
+
+export const Big: StoryObj<TButtonProps> = {
+  args: {
+    size: 'big',
+  },
+};
