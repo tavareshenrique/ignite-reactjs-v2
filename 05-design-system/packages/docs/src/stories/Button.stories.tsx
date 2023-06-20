f51045ed0bcea38a2852669a0a30765918ae2370
Henrique Tavares <ihenrits@gmail.com>
2023-06-20 09:38:29 -0300

    docs: tokens docs in storybook

--- 05-design-system/packages/docs/src/stories/Button.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import type { StoryObj, Meta } from '@storybook/react';
 import { Button, TButtonProps } from '@ignite-ui/react';
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
