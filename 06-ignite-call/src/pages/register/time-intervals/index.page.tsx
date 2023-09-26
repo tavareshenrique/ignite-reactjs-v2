59ab4a65cfc47169c3e36fd96d84633cdca3f939
Henrique Tavares <ihenrits@gmail.com>
2023-09-26 07:17:08 -0300

    validation and transformation with zod

--- 06-ignite-call/src/pages/register/time-intervals/index.page.tsx
+++ 06-ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -8,17 +8,41 @@ import {
 } from '@ihenrits-ui/react';
 import { ArrowRight } from 'phosphor-react';
 
-import { Container, Header } from '../styles';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
 
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { getWeekDays } from '../../../utils/get-week-days';
+
+import { Container, Header } from '../styles';
 import {
   IntervalBox,
   IntervalContainer,
   IntervalDay,
   IntervalInputs,
   IntervalItem,
+  FormError,
 } from './styles';
-import { getWeekDays } from '../../../utils/get-week-days';
-import { Controller, useFieldArray, useForm } from 'react-hook-form';
+
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana',
+    }),
+});
+
+type TTimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>;
 
 export default function TimeIntervals() {
   const {
@@ -28,6 +52,7 @@ export default function TimeIntervals() {
     watch,
     formState: { isSubmitting, errors },
   } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -50,7 +75,9 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals');
 
-  async function handleSetTimeIntervals() {}
+  async function handleSetTimeIntervals(data: TTimeIntervalsFormData) {
+    console.log(data);
+  }
 
   return (
     <Container>
@@ -107,7 +134,11 @@ export default function TimeIntervals() {
           })}
         </IntervalContainer>
 
-        <Button type="submit">
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.message}</FormError>
+        )}
+
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
