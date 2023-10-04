f1332362fa8f8abb7b83296756cff8b53a2a0534
Henrique Tavares <ihenrits@gmail.com>
2023-10-04 06:51:43 -0300

    schedulings table

--- 06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ 06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import dayjs from 'dayjs';
+
 import { Calendar } from '../../../../../components/Calendar';
 
 import {
@@ -15,6 +17,11 @@ export function CalendarStep() {
 
   const isDateSelected = !!selectedDate;
 
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null;
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null;
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
@@ -22,7 +29,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay}, <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
