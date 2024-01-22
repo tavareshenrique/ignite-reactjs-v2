238e761a4958e154866bb4c117c8a86bf2844d03
Henrique Tavares <ihenrits@gmail.com>
2024-01-22 06:45:51 -0300

    start: new project with default configs

---
+++ 07-pizzashop-web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+import './global.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  )
+}
+
+export default App
