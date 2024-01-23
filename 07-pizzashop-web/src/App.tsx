be42510386df68b7ce253e9425e53a81ae545a63
Henrique Tavares <ihenrits@gmail.com>
2024-01-23 06:29:16 -0300

    feat: instaling react router dom

--- 07-pizzashop-web/src/App.tsx
+++ 07-pizzashop-web/src/App.tsx
@@ -1,13 +1,11 @@
 import './global.css';
 
-import { Button } from './components/ui/button';
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes';
 
 function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
