1ba231372418297cac7c06260fb7320bec556ecb
Henrique Tavares <ihenrits@gmail.com>
2024-01-24 06:21:47 -0300

    feat: add sonner notification toast

--- 07-pizzashop-web/src/pages/auth/sign-in.tsx
+++ 07-pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -23,6 +24,13 @@ export function SignIn() {
     console.log(data);
 
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    toast.success('Enviamos um link de autenticação para seu e-mail!', {
+      action: {
+        label: 'Reenviar e-mail',
+        onClick: () => handleSignIn(data),
+      },
+    });
   }
 
   return (
