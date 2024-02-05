8642f8db36fd7fc3557eb96998f2b59ca987fb86
Henrique Tavares <ihenrits@gmail.com>
2024-02-05 06:34:53 -0300

    feat: logout

--- 07-pizzashop-web/src/components/account-menu.tsx
+++ 07-pizzashop-web/src/components/account-menu.tsx
@@ -1,13 +1,14 @@
-import { DialogTrigger } from '@radix-ui/react-dialog';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { Building, ChevronDown, LogOut } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { getManagedRestaurant } from '@/api/get-managed-restaurant';
 import { getProfile } from '@/api/get-profile';
+import { signOut } from '@/api/sign-out';
 
 import { StoreProfileDialog } from './store-profile-dialog';
 import { Button } from './ui/button';
-import { Dialog } from './ui/dialog';
+import { Dialog, DialogTrigger } from './ui/dialog';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,6 +20,8 @@ import {
 import { Skeleton } from './ui/skeleton';
 
 export function AccountMenu() {
+  const navigate = useNavigate();
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -31,6 +34,13 @@ export function AccountMenu() {
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     });
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true });
+    },
+  });
 
   return (
     <Dialog>
@@ -48,8 +58,9 @@ export function AccountMenu() {
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
+
         <DropdownMenuContent align="end" className="w-56">
-          <DropdownMenuLabel className="flex flex-col ">
+          <DropdownMenuLabel className="flex flex-col">
             {isLoadingProfile ? (
               <div className="space-y-1.5">
                 <Skeleton className="h-4 w-32" />
@@ -64,19 +75,22 @@ export function AccountMenu() {
               </>
             )}
           </DropdownMenuLabel>
-
           <DropdownMenuSeparator />
-
           <DialogTrigger asChild>
             <DropdownMenuItem>
-              <Building className="m-2 h-4 w-4" />
-              <span>Perfil da Loja</span>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="m-2 h-4 w-4" />
-            <span>Sair</span>
+          <DropdownMenuItem
+            asChild
+            disabled={isSigningOut}
+            className="text-rose-500 dark:text-rose-400"
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
