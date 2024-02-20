d44c6bad6b211a6eabbff6c908ecce1c07fb9671
Henrique Tavares <ihenrits@gmail.com>
2024-02-20 07:09:51 -0300

    test: add status unit test

--- 07-pizzashop-web/src/components/order-status.tsx
+++ 07-pizzashop-web/src/components/order-status.tsx
@@ -16,19 +16,31 @@ export function OrderStatus({ status }: IOrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          className="h-2 w-2 rounded-full bg-slate-400"
+          data-testid="badge"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-rose-500"
+          data-testid="badge"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-emerald-500"
+          data-testid="badge"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-amber-500"
+          data-testid="badge"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
--- 07-pizzashop-web/vite.config.ts
+++ 07-pizzashop-web/vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'node:path';
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, type UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
