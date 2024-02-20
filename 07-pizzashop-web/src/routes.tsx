b7538c71003e588ba9ea63b3a19b361148ee63fd
Henrique Tavares <ihenrits@gmail.com>
2024-02-20 06:39:52 -0300

    feat: improve error and 404 page

--- 07-pizzashop-web/src/routes.tsx
+++ 07-pizzashop-web/src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashbord/dashboard';
 import { Orders } from './pages/app/orders/orders';
 import { SignIn } from './pages/auth/sign-in';
 import { SignUp } from './pages/auth/sign-up';
+import { Error } from './pages/error';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -25,5 +26,9 @@ export const router = createBrowserRouter([
       { path: '/sign-in', element: <SignIn /> },
       { path: '/sign-up', element: <SignUp /> },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ]);
