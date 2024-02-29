78581547638f6f145be21bfc78564bf8c2571475
Henrique Tavares <ihenrits@gmail.com>
2024-02-29 06:24:13 -0300

    feat: msw setup

--- 07-pizzashop-web/src/env.ts
+++ 07-pizzashop-web/src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+  MODE: z.enum(['development', 'production', 'test']),
   VITE_API_URL: z.string().url(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 });
